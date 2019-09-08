7a8a486bb15dcd3476fb6aaa0f75b8c5f1a12c33
tyler-omgcobra <tyler@omgcobra.org>
2019-09-08 19:08:32 -0400

    Make RecipeBehaviorTest much better

--- app/src/androidTest/java/org/omgcobra/matchthese/test/RecipeBehaviorTest.kt
+++ app/src/androidTest/java/org/omgcobra/matchthese/test/RecipeBehaviorTest.kt
@@ -1,5 +1,7 @@
 package org.omgcobra.matchthese.test
 
+import android.view.View
+import androidx.appcompat.widget.AppCompatCheckedTextView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
@@ -10,12 +12,17 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
+import org.hamcrest.Matcher
+import org.hamcrest.core.AllOf.allOf
+import org.hamcrest.core.Is.isA
 import org.hamcrest.core.StringStartsWith
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.omgcobra.matchthese.MainActivity
+import org.omgcobra.matchthese.MatchTheseApplication
 import org.omgcobra.matchthese.R
 import java.util.*
 
@@ -36,35 +43,92 @@ class RecipeBehaviorTest {
     }
 
     @Test
-    fun addItem_sameActivity() {
+    fun recipeTest() {
+        addRecipe()
+        checkIngredientHasRecipe()
+        deleteRecipe()
+        deleteIngredient()
+    }
+
+    private fun addRecipe() {
+        recipeView()
+
         onView(withId(R.id.floating_action_button_recipe))
                 .perform(click())
+
         onView(withId(R.id.edit_name))
                 .check(matches(isDisplayed()))
                 .perform(typeText(name))
+
         onView(withId(R.id.edit_list))
                 .check(matches(isDisplayed()))
                 .perform(typeText("$ingredient,"))
+
         onView(withId(R.id.action_save))
                 .check(matches(isDisplayed()))
                 .perform(click())
+    }
 
-        onView(withText(name))
+    private fun checkIngredientHasRecipe() {
+        ingredientView()
+
+        onView(allOf(withId(R.id.main_text), withText("$ingredient (1)")))
+                .check(matches(isDisplayed()))
+
+        onView(allOf(withId(R.id.sub_text), withText(name)))
+                .check(matches(isDisplayed()))
+    }
+
+    private fun deleteRecipe() {
+        recipeView()
+
+        val mainTextWithName = allOf(withId(R.id.main_text), withText(name))
+        onView(mainTextWithName)
                 .check(matches(isDisplayed()))
                 .perform(swipeLeft())
-        onView(withText(name))
-                .check(doesNotExist())
 
+        recipeView()
+
+        onView(mainTextWithName)
+                .check(doesNotExist())
+    }
+
+    private fun deleteIngredient() {
+        ingredientView()
+
+        val mainTextStartsWith = allOf(withId(R.id.main_text), withText(StringStartsWith(ingredient)))
+        onView(mainTextStartsWith)
+                .check(matches(isDisplayed()))
+                .perform(swipeLeft())
+
+        ingredientView()
+
+        onView(mainTextStartsWith)
+                .check(doesNotExist())
+    }
+
+    private fun clickDrawerItem(textId: Int) {
         onView(withId(R.id.drawer_layout))
                 .perform(DrawerActions.open())
                 .check(matches(DrawerMatchers.isOpen()))
 
-        onView(withText(R.string.ingredients_title))
+        onView(allOf(withText(textId), isA(AppCompatCheckedTextView::class.java) as Matcher<View>))
                 .perform(click())
-        onView(withText(StringStartsWith(ingredient)))
-                .check(matches(isDisplayed()))
-                .perform(swipeRight())
-        onView(withText(ingredient))
-                .check(doesNotExist())
+    }
+
+    private fun recipeView() = clickDrawerItem(R.string.recipes_title)
+    private fun ingredientView() = clickDrawerItem(R.string.ingredients_title)
+
+    @After
+    fun cleanup() {
+        val db = MatchTheseApplication.getDB()
+        val ingredientDao = db.ingredientDao()
+        val recipeDao = db.recipeDao()
+
+        val recipe = recipeDao.getByName(name)
+        recipe?.let { recipeDao.delete(it) }
+
+        val ingredient = ingredientDao.getByName(ingredient)
+        ingredient?.let { ingredientDao.delete(it) }
     }
 }
